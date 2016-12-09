0ea6c86500257340ddf89a03a651beef12e7737e
RedbackThomson <redbackthomson@gmail.com>
2016-12-08 23:22:58 -0800

    Added side hexagons and made edges

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 
 var paths = {
   scss: ['scss/main.scss'],
@@ -11,6 +12,10 @@ gulp.task('build-sass', function() {
   return gulp.src(paths.scss)
     .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(sourcemaps.write(''))
     .pipe(gulp.dest(paths.css));
 });

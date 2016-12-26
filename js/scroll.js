738503a61a58027aefcb7e79e9c6f2a600a9648f
liawesomesaucer <vincent.oddrepublic@gmail.com>
2016-12-26 11:23:12 +0800

    Kind of done with nav for now

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,17 @@
+var sectionIds = ["#about", "#resources", "#schedule", "#faq"];
+
 $(document).ready(function() {
+  $(".sd-nav--fixed").hide();
+
   $(window).scroll(function() {
-    if ($(window).top > 1000) {
-      
+    if ($(window).scrollTop() > 0) {
+      $(".sd-nav--fixed").slideDown();
+    } else {
+      $(".sd-nav--fixed").slideUp();
     }
+
+
   });
+
+  // Todo: organize per section
 });
